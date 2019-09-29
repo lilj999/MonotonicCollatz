51749f08b8bdfc04e2e746f29aae11e7125f8e49
lilj999 <lilj999@users.noreply.github.com>
2019-09-29 11:50:42 +0800

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -40,13 +40,15 @@ def S(n, record=False):
 #   The Function of Monotonic(n, K1, K2, K3) is to calculate monotonic increasing series of any limited length $n$ with the same step size 1.
 #   Explainations:         
 #   1) All number like "K * 2^(n+1)-1" can generate a monotonic increasing series of length n;
-#   2) Given K, the serie starting with  K*2^(n+1)-1 ending with 4 * K * 3^(n-1) -1;
-#   3) Monotonic(n,0) is the minimal x_1 that satisfies the monotonic constraints with the step size 1;
-#   4) The function compares three series with (K1,K2,K3), and draw them side by side in a window.
+#   2) Given K, the serie starting with  K*2^(n+1)-1 ending with 4 * K * 3^(n-1) -1, so 
+#      the jump height of the  Monotonic series is  (4 * K * 3^(n-1) -(K+1)*2^n);
+#   3) K=0 is corresponding to the minimal x_1 that satisfies the monotonic constraints with the step size 1;
+#   4) The Monotonic series is the series that are increasing fastest for any given $n$;
+#   5) The function compares three series with (K1,K2,K3), and draw them side by side in a window.
 #      
 #   Example:  
-#      Monotonic(10,2) compares three series starting with (3 * 2^11-1, 4* 2^11-1, 5* 2^11-1).
-#      Monotonic(199,3) compares three series starting with (4 * 2^200-1, 5* 2^200-1, 6* 2^200-1).
+#      Monotonic(10,3,4,5) compares three series starting with (3 * 2^11-1, 4* 2^11-1, 5* 2^11-1).
+#      Monotonic(199,4,5,6) compares three series starting with (4 * 2^200-1, 5* 2^200-1, 6* 2^200-1).
 #             
 ############
 def Monotonic(n, K1=0, K2=1, K3=2):
@@ -80,5 +82,5 @@ def Monotonic(n, K1=0, K2=1, K3=2):
     plt.show()
 
 # examples
-#Monotonic(10,2)
-Monotonic(199,3)
+#Monotonic(10,1,2,3)
+Monotonic(199,3,4,5)
